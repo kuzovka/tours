9cdfa1bbe7cf997760b5da753c02e82bd7f8b803
kuzovka <kuzova.irinka@gmail.com>
2023-02-11 17:00:51 +0300

    начало работы по бронированию

--- src/app.js
+++ src/app.js
@@ -86,20 +86,77 @@ function renderTours(tours) {
                              </div>
                          </div>
                          <div class="btn-container">
-                             <button class="btn" id="openModalWindowButton-${
-                                 tour.id
+                             <button class="btn" id="openModalButton-${tour.id}"
                              }">Забронировать</button>
                              <button class="btn">Добавить в избранное</button>
                          </div>
                     </div>
         `
     })
+// открытие модального окна для бронирования
+    tours.forEach((tour) => {
+        document.getElementById(`openModalButton-${tour.id}`).addEventListener("click", () =>
+        openModalWindow(tour.id))
+    })
+}
+
+const modalWindow = document.getElementById("modalWindow")
+const closeModalWindow = document.getElementById("CloseModalWindowButton")
+const closeModalXButton = document.getElementById("modal-btn")
+
+closeModalWindow.addEventListener('click', closeModal)
+closeModalXButton.addEventListener('click', closeModal)
+
+function closeModal() {
+    modalWindow.style.display = "none"
+}
+
+let currentId
+
+// НЕ ПОЛУЧИЛОСЬ ОТОБРАЖАТЬ ДАННЫЕ ТУРА
+async function openModalWindow(id) {
+
+    const response = await fetch('https://www.bit-by-bit.ru/api/student-projects/tours');
+    const tours = await response.json()
+
+    modalWindow.style.display = "flex"
+
+    const tour = tours.find(t => i.id === id)
+    document.getElementById("tourCard").innerHTML = ""
+    document.getElementById("tourCard").innerHTML += `
+    <div>
+            <div class="h-12">
+                <a class="font-semibold text-yellow-600 hover:underline" id="hotelName">название отеля</a>
+            </div>
+            <p class="font-normal text-sky-900 mb-2 mt-2" id="country">страна</p>
+        </div>
+        <div id="image">изображение</div>
+        <div class="my-5 text-sky-800 flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
+            </svg>
+            <span class="ml-3" id="startTime">дата</span>
+            <span class="mr-1 ml-1"> - </span>
+            <span id="endTime"> дата</span>
+
+        </div>
+        <div>
+            <p class="font-normal text-gray-800 text-m" id="price">Стоимость тура:</p>
+            <div class="flex items-center mt-3">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-yellow-500 mr-3">
+                    <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd" />
+                </svg>
+                <p class="font-normal text-gray-800 text-m" id="rating">рейтинг</p>
+            </div>
+    </div>
+
+    `
+    currentId = id
+
 }
 
 //  dropdown по стране и рейтингу
-document
-    .querySelectorAll(".dropdown__container")
-    .forEach(function (dropdownWrapper) {
+document.querySelectorAll(".dropdown__container").forEach(function (dropdownWrapper) {
         const dropdownBtn = dropdownWrapper.querySelector(".dropdownButton")
         const dropdownList = dropdownWrapper.querySelector(".dropdownList")
         const dropdownListItems = dropdownList.querySelectorAll(
@@ -108,21 +165,21 @@ document
 
         // откыть dropdowns
         dropdownBtn.addEventListener("click", function () {
-            dropdownList.style.display = "block"
+            dropdownList.classList.remove('hidden')
         })
 
         // заполнить dropdowns выбранным значением и закрыть список
         dropdownListItems.forEach(function (listItem) {
             listItem.addEventListener("click", function () {
                 dropdownBtn.innerText = this.innerText
-                dropdownList.style.display = "none"
+                dropdownList.classList.add('hidden')
             })
         })
 
         // скрыть dropdown если клик не по кнопке
         document.addEventListener("click", function (event) {
             if (event.target !== dropdownBtn) {
-                dropdownList.style.display = "none"
+                dropdownList.classList.add('hidden')
             }
         })
     })
@@ -234,5 +291,34 @@ window.addEventListener("load", () => {
         loader.remove()
     }, 1000)
 })
+
+// отправка формы пронирования. НЕ ЗАВЕРШЕНО
+document.getElementById('form').addEventListener("submit", sendFormData)
+
+async function sendFormData(event) {
+    event.preventDefault()
+
+    let name = document.getElementById('inputName')
+    let phone = document.getElementById('inputTel')
+    let email = document.getElementById('inputEmail')
+    let comment = document.getElementById('textarea')
+
+    const params = {
+        customerName: name.value,
+        phone: phone.value,
+        email: email.value,
+        description: comment.value
+    }
+
+    const url = `https://www.bit-by-bit.ru/api/student-projects/tours/${currentId}`
+
+    let response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(params)
+    })
+
+
+
+}
 
 init()
