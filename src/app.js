08fa8911d2445a9c9cb3fcf3a73ac8f50bc43927
kuzovka <kuzova.irinka@gmail.com>
2023-02-15 17:06:58 +0300

    отображение current tour

--- src/app.js
+++ src/app.js
@@ -113,7 +113,7 @@ function closeModal() {
 
 let currentId
 
-// НЕ ПОЛУЧИЛОСЬ ОТОБРАЖАТЬ ДАННЫЕ ТУРА
+// окно для бронирования
 async function openModalWindow(id) {
 
     const response = await fetch('https://www.bit-by-bit.ru/api/student-projects/tours');
@@ -121,34 +121,54 @@ async function openModalWindow(id) {
 
     modalWindow.style.display = "flex"
 
-    const tour = tours.find(t => i.id === id)
+    const currentTour = tours.find(t => t.id === id)
     document.getElementById("tourCard").innerHTML = ""
     document.getElementById("tourCard").innerHTML += `
-    <div>
-            <div class="h-12">
-                <a class="font-semibold text-yellow-600 hover:underline" id="hotelName">название отеля</a>
-            </div>
-            <p class="font-normal text-sky-900 mb-2 mt-2" id="country">страна</p>
-        </div>
-        <div id="image">изображение</div>
-        <div class="my-5 text-sky-800 flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
-            </svg>
-            <span class="ml-3" id="startTime">дата</span>
-            <span class="mr-1 ml-1"> - </span>
-            <span id="endTime"> дата</span>
+                         <div>
+                             <img
+                                 class="h-72 w-full  rounded-md mb-5"
+                                 src="${currentTour.image}"
+                                 alt=""/>
+                        </div>
+                        <div>
+                            <a class="font-semibold text-yellow-600 hover:underline">${currentTour.hotelName}</a>
+                        </div>
+                        <p class="font-normal text-sky-900 my-2">
+                                ${currentTour.city ? `<a href="#">${currentTour.city},</a>` : ""}
+                                <a href="#">${currentTour.country}</a>
+                                </p>
+                           </div>
+                         <div class=" text-sky-800 flex items-center">
+                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
+                               </svg>
+                             <span class="ml-3">${format(
+                                 new Date(currentTour.startTime),
+                                 "dd.MM.yyyy",
+                                 { locale: ru }
+                             )}</span>
+                             <span class="mr-1 ml-1"> - </span>
+                             <span> ${format(
+                                 new Date(currentTour.endTime),
+                                 "dd.MM.yyyy",
+                                 { locale: ru }
+                             )}</span>
 
-        </div>
-        <div>
-            <p class="font-normal text-gray-800 text-m" id="price">Стоимость тура:</p>
-            <div class="flex items-center mt-3">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-yellow-500 mr-3">
-                    <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd" />
-                </svg>
-                <p class="font-normal text-gray-800 text-m" id="rating">рейтинг</p>
-            </div>
-    </div>
+                        </div>
+
+                         <div>
+                             <p class="font-normal text-gray-800 text-m my-2">
+                             Стоимость тура: ${currentTour.price.toLocaleString("ru", {
+                                 style: "currency",
+                                 currency: "rub",
+                                 currencyDisplay: "code",
+                                 minimumFractionDigits: 0
+                             })}
+                             </p>
+
+                         </div>
+
+
 
     `
     currentId = id
@@ -293,7 +313,7 @@ window.addEventListener("load", () => {
 })
 
 // отправка формы пронирования. НЕ ЗАВЕРШЕНО
-document.getElementById('form').addEventListener("submit", sendFormData)
+document.getElementById('bookingTourButton').addEventListener("click", sendFormData)
 
 async function sendFormData(event) {
     event.preventDefault()
@@ -301,7 +321,7 @@ async function sendFormData(event) {
     let name = document.getElementById('inputName')
     let phone = document.getElementById('inputTel')
     let email = document.getElementById('inputEmail')
-    let comment = document.getElementById('textarea')
+    let comment = document.getElementById('textArea')
 
     const params = {
         customerName: name.value,
@@ -316,6 +336,29 @@ async function sendFormData(event) {
         method: "POST",
         body: JSON.stringify(params)
     })
+
+    try {
+        let data = await response.json()
+
+        if (data) {
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Тур успешно забронирован, мы сроко свяжемся с Вами!',
+                showConfirmButton: false,
+                timer: 3000
+              })
+
+        }
+    } catch {
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'Проверьте корректность введенных Вами данных!!',
+            showConfirmButton: false,
+            timer: 3000
+          })
+    }
 
 
 
